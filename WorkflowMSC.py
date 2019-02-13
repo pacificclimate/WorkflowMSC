c1b43b786fd3f118bd7d4c632267e75ac2d89d84
nannau <nannau@uvic.ca>
2019-02-13 14:59:29 -0800

    Further plotting of percentiles. Percentiles for July appear to be working well.

--- WorkflowMSC.py
+++ WorkflowMSC.py
@@ -11,6 +11,8 @@ import numpy as np
 import pandas as pd
 
 class WorkflowMSC:
+    def __init__():
+        print("Initiated!")
 
     def baseline(start_time, end_time):
         """
@@ -44,7 +46,53 @@ class WorkflowMSC:
         """
         if start_year.year - end_year.year < 1:
             raise ValueError("Annual precipitation value requires a time window of at least 1 year.")
-        
-        query = session.query(func.sum(Obs.datum).label("sum"), func.max(Obs.time).label("time"), History.lat, History.lon, History.station_id).group_by(History).filter(Variable.standard_name == "lwe_thickness_of_precipitation_amount").filter(or_(Variable.description == "Total precipiation", Variable.id == 1397))
+
+        yr_interval = float(np.abs(end_year.year - start_year.year))
+        print(yr_interval)
+        query = session.query(func.sum(Obs.datum*0.1/yr_interval).label("sum"), func.max(Obs.time).label("max_date"), History.lat, History.lon, History.station_id).group_by(History).having(func.max(Obs.time) >= end_year).filter(Variable.standard_name == "lwe_thickness_of_precipitation_amount").filter(or_(Variable.description == "Total precipiation", Variable.id == 1397))
         query = query.filter(Obs.time <= end_year).filter(Obs.time >= start_year).join(History).join(Variable)
+        return query
+    
+    def baseline(start_time, end_time):
+        query = session.query(Obs.datum, Obs.time, Variable.standard_name, History.station_id).filter(Obs.time <= end_time).filter(Obs.time >= start_time).join(History).join(Variable)
+        return query
+
+    def precip_query(start_time, end_time):
+        query = baseline(start_time, end_time).filter(Variable.standard_name == "lwe_thickness_of_precipitation_amount").filter(or_(Variable.description == "Total precipiation", Variable.id == 1397))
+        return query
+
+    def design_temp_25(start_time, end_time, month = 1):
+
+        query = session.query(func.percentile_cont(0.025).within_group(
+                        Obs.datum.asc()).label("temp"), 
+                        func.min(Obs.time).label("time"),
+                        History.lat,
+                        History.lon,
+                        History.station_id).group_by(History)\
+                        .filter(or_(Variable.description == "Air temperature", Variable.id == 1510)) \
+        .filter(Variable.standard_name == "air_temperature") \
+        .filter(and_(func.extract("month", Obs.time) == month, func.extract("year", Obs.time) <= end_time.year)) \
+        .filter(func.extract("year", Obs.time) >= start_time.year)
+
+        return query
+
+    def design_temp_1(start_time, end_time, month = 1):
+
+        query = session.query(func.percentile_cont(0.01).within_group(
+                        Obs.datum.asc()).label("temp"), 
+                        func.min(Obs.time).label("time"),
+                        History.lat,
+                        History.lon,
+                        History.station_id).group_by(History) \
+        .filter(or_(Variable.description == "Air temperature", Variable.id == 1510)) \
+        .filter(Variable.standard_name == "air_temperature") \
+        .filter(and_(func.extract("month", Obs.time) == month, func.extract("year", Obs.time) <= end_time.year)) \
+        .filter(func.extract("year", Obs.time) >= start_time.year)
+
+        return query
+
+    def show_design_values():
+
+        query = session.query(Variable.standard_name, Variable.description, Variable.unit, Variable.id)
+
         return query
